dfc1cbef7847a6415645c9b170aaff03501fa5a3
cpak00 <chenyiming123@126.com>
2018-07-05 11:25:24 +0800

    Update login.sh

--- THU/login.sh
+++ THU/login.sh
@@ -4,4 +4,4 @@ read username
 echo "输入密码"
 read password
 password_md5=`echo -n ${password}|md5sum|cut -d ' ' -f1`
-echo `wget -q -O - --post-data="action=login&username=${username}&password={MD5_HEX}df3ccb2be2c283de9930369b90b132ae&ac_id=1" "http://net.tsinghua.edu.cn/do_login.php"`
+echo `wget -q -O - --post-data="action=login&username=${username}&password={MD5_HEX}${password_md5}&ac_id=1" "http://net.tsinghua.edu.cn/do_login.php"`
